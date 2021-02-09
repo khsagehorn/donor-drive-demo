d87ec0baaa49c0a7c09babea5d80c5bab33ce677
Hunter Sagehorn <hunter.sagehorn@moxiesozo.com>
2021-02-08 23:57:59 -0500

    views js

--- dist/index.php
+++ dist/index.php
@@ -10,12 +10,12 @@
       <div class="util flex space-between">
         <h1>Stream</h1>
         <div class="views">
-          <input type="radio" name="views" value="stream-chat" aria-label="Stream and Chat">
-          <input type="radio" name="views" value="stream-only" aria-label="Stream Only">
-          <input type="radio" name="views" value="chat-only" aria-label="Chat Only">
+          <input type="radio" name="view" value="stream-chat" aria-label="Stream and Chat" checked="checked">
+          <input type="radio" name="view" value="stream-only" aria-label="Stream Only">
+          <input type="radio" name="view" value="chat-only" aria-label="Chat Only">
         </div>
       </div>
-      <div class="main-window flex stream-chat">
+      <div id="main-window" class="stream-chat">
         <div class="video">
           <?php
             include 'functions.php';
@@ -39,6 +39,11 @@
       </div>
     </section>
 
-    <link rel="javascript" href="assets/scripts.js">
+    <script
+      src="https://code.jquery.com/jquery-3.5.1.min.js"
+      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+      crossorigin="anonymous">
+    </script>
+    <script src="scripts.js"></script>
   </body>
 </html>
---
+++ dist/scripts.js
@@ -0,0 +1,6 @@
+(function(){
+  $('input[name=view]').change(function() {
+    let className = $('input[name=view]:checked').val();
+    $("#main-window").attr('class', className);
+  });
+})();
