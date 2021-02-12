1f7fd98de8d7489951693e9088136f7ffd01446e
Hunter Sagehorn <hunter.sagehorn@moxiesozo.com>
2021-02-12 16:17:28 -0500

    rn

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="assets/styles.css">
   </head>
   <body>
-    <?php include 'sections/viewer.php'; ?>
+    <?php include './sections/viewer.php'; ?>
     <script
       src="https://code.jquery.com/jquery-3.5.1.min.js"
       integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
